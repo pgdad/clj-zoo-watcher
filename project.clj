2638fc6abfb2bf1e559545b80a29d3083aaf8943
pgdad <pgdad@hotmail.com>
2012-04-24 18:05:33 -0400

    new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-zoo-watcher "1.0.5"
+(defproject clj-zoo-watcher "1.0.6"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-tree-zipper "1.0.0"]
