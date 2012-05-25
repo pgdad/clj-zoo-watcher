9c0cf6fd31be0ae3a671c08a4819a32c4d51f188
pgdad <pgdad@hotmail.com>
2012-05-25 08:44:12 -0400

    compile warn-on-reflection

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clj-zoo-watcher "1.0.9"
+(defproject clj-zoo-watcher "1.0.10"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-tree-zipper "1.0.0"]
                  [zookeeper-clj "0.9.2"]]
   :repl-init clj-zoo-watcher.core
-  :aot [clj-zoo-watcher.core clj-zoo-watcher.multi])
+  :aot [clj-zoo-watcher.core clj-zoo-watcher.multi]
+  :warn-on-reflection true
+  :jar-exclusions [#"project.clj"]
+  :omit-source true)
