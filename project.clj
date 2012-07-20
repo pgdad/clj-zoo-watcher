850fe0061dd5f671c2d98a87b6638021c93bf190
Esa Laine <esa.laine@chase.com>
2012-07-20 11:33:35 -0400

    wif

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject clj-zoo-watcher "1.0.10"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.netflix.curator/curator-recipes "1.1.13"]
-                 [zookeeper-clj "0.9.2"]]
+                 [com.netflix.curator/curator-recipes "1.1.13"]]
   :plugins [[lein-swank "1.4.4"]]
   :repl-init clj-zoo-watcher.core
   ; :aot :all
